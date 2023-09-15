1f3f6597118aa64ea5ba4f01a0eee73dbf0257c2
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2023-09-15 14:27:02 -0400

    Decode window URL before attempting to match it against route patterns

--- src/js/Route.js
+++ src/js/Route.js
@@ -82,7 +82,7 @@ export default class Route {
 
         const [location, query] = url.replace(/^\w+:\/\//, '').split('?');
 
-        const matches = new RegExp(`^${pattern}/?$`).exec(location);
+        const matches = new RegExp(`^${pattern}/?$`).exec(decodeURI(location));
 
         if (matches) {
             for (const k in matches.groups) {
